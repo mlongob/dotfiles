22bc2ce5c5d5a83f2bf0d1e8fa214f0551066f76
Mario Longobardi <mlongobardi2@bloomberg.net>
2015-09-08 10:57:54 -0400

    Add more Ubuntu dependencies

--- install_ubuntu_dependencies.sh
+++ install_ubuntu_dependencies.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 sudo apt-get install \
-    git \
-    tmux \
+    autoconf \
+    automake \
+    autotools-dev \
     build-essential \
+    chromium-browser \
+    clang-format-3.6 \
+    clipman \
     cmake \
+    curl \
+    git \
+    haskell-platform \
+    htop \
+    java-jdk \
+    libgtest-dev \
+    maven \
+    openjdk-7-jdk \
     python-dev \
+    rlwrap \
+    shellcheck \
+    sshfs \
+    tmux \
+    tree \
     vim-gtk \
     xclip \
+    xfce4-clipman \
     xsel \
-    shellcheck \
-    htop \
     zsh
