6bd5d55190a0b613840df680f3297b5c6912980e
Mario Longobardi <mlongobardi2@bloomberg.net>
2016-06-23 09:46:26 -0400

    Remove stack/ghc path

--- zsh/bootstrap.zsh
+++ zsh/bootstrap.zsh
@@ -1,10 +1,3 @@
 PATH=~/.dotfiles/bin:${PATH}
-
-# Set paths for Haskell Stack
-ghc_path=$(echo /home/mlongob/.stack/programs/*/*/bin)
-if [ $? -eq 0 ]; then
-    PATH=$ghc_path:${PATH}
-fi
-unset ghc_path
 PATH=~/.local/bin:${PATH}
 export PATH
